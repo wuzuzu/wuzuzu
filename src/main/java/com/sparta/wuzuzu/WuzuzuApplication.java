fb53cae51b18cd9b173091956b529c731fd533ca
DAchoMI <dachomi97@gmail.com>
2024-03-27 17:32:21 +0900

    :tada: Init

---
+++ src/main/java/com/sparta/wuzuzu/WuzuzuApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.wuzuzu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuzuzuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuzuzuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/wuzuzu/WuzuzuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.wuzuzu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuzuzuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
