cd3addfb48e1a0c77a9409e8324e7aa783a46611
pastjung <chwogus0303@naver.com>
2024-03-30 01:22:48 +0900

    feat: requestDto Valid 검증 예외처리 추가

--- src/main/java/com/sparta/wuzuzu/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/wuzuzu/global/exception/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ import com.sparta.wuzuzu.domain.common.dto.CommonResponse;
 import com.sparta.wuzuzu.domain.common.dto.ExceptionResponse;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,6 +19,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(Exception ex) {
         ExceptionResponse response = ExceptionResponse.builder()
             .msg(ex.getMessage())
+            .httpCode(HttpStatus.BAD_REQUEST.value())
+            .build();
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        ExceptionResponse response = ExceptionResponse.builder()
+            .msg(Objects.requireNonNull(ex.getFieldError()).getDefaultMessage())
             .httpCode(HttpStatus.BAD_REQUEST.value())
             .build();
         return ResponseEntity.badRequest().body(response);
