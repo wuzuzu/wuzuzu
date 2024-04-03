2d30dda6126ef0e6164ed289804b73bc78ba22ad
9noeyni9 <koo949494@naver.com>
2024-04-04 08:52:50 +0900

    :sparkles: feat: admin 권한으로 user 전체 조회 구현

--- src/main/java/com/sparta/wuzuzu/domain/admin/controller/AdminController.java
+++ src/main/java/com/sparta/wuzuzu/domain/admin/controller/AdminController.java
@@ -1,23 +1,40 @@
 package com.sparta.wuzuzu.domain.admin.controller;
 
+import com.sparta.wuzuzu.domain.admin.dto.UserInformReadResponse;
 import com.sparta.wuzuzu.domain.admin.service.AdminService;
 import com.sparta.wuzuzu.domain.common.dto.CommonResponse;
+import com.sparta.wuzuzu.domain.user.dto.MyInformReadResponse;
+import com.sparta.wuzuzu.domain.user.entity.UserRole;
 import com.sparta.wuzuzu.global.jwt.JwtTokenBlacklist;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.sparta.wuzuzu.domain.user.entity.UserRole.ADMIN;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admins")
 public class AdminController {
 
+    private final AdminService adminService;
     private final JwtTokenBlacklist jwtTokenBlacklist;
+
+    @Secured("ADMIN")
+    @GetMapping("/users")
+    public ResponseEntity<CommonResponse<List<UserInformReadResponse>>> readAllUsers(){
+        List<UserInformReadResponse> userList = adminService.readAllUsers();
+        return CommonResponse.ofDataWithHttpStatus(userList, HttpStatus.OK);
+    }
 
     @PostMapping("/logout")
     public ResponseEntity<CommonResponse<String>> logout(HttpServletRequest request) {
--- src/main/java/com/sparta/wuzuzu/domain/admin/service/AdminService.java
+++ src/main/java/com/sparta/wuzuzu/domain/admin/service/AdminService.java
@@ -1,5 +1,22 @@
 package com.sparta.wuzuzu.domain.admin.service;
 
+import com.sparta.wuzuzu.domain.admin.dto.UserInformReadResponse;
+import com.sparta.wuzuzu.domain.user.entity.User;
+import com.sparta.wuzuzu.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
 
+    private final UserRepository userRepository;
+
+    public List<UserInformReadResponse> readAllUsers() {
+        List<User> userList = userRepository.findAll();
+        return userList.stream().map(UserInformReadResponse::new).collect(Collectors.toList());
+    }
 }
