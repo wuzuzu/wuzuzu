1449202341f92ab8e52475a3f077622cbd60a4ca
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-04-03 20:18:37 +0900

    Feat/inyeong user auth (#8)
    * :sparkles: feat: 비밀번호 수정 구현
    
    - 기존 비밀번호 일치 해야 수정 가능
    - 새로운 비밀번호, 비밀번호 확인 후 수정 가능
    - validated annotation controller에 추가
    - CommonResponse 사용 확인 필요
    
    * :sparkles: feat: 인증 메일 발송 구현
    
    - build.gradle 의존성 추가 (주석 표시)
    - html 파일 jquery, thymeleaf 추가
    
    * :sparkles: feat: 코드 일치 후 인증 구현
    
    - 이메일 인증 기능만 구현된 상태
    - 추후에 회원가입 시 이메일 인증 후 회원가입 되도록 구현 예정
    
    * :sparkles: feat: RefreshToken 구현
    
    * :sparkles: feat: 로그아웃 기능 구현
    
    * :recycle: refactor: 불필요한 import문 삭제, UserResponse dto, Email entity 삭제

---
+++ src/main/java/com/sparta/wuzuzu/domain/email/dto/EmailRequest.java
@@ -0,0 +1,10 @@
+package com.sparta.wuzuzu.domain.email.dto;
+
+import lombok.Getter;
+
+@Getter
+public class EmailRequest {
+
+    // 이메일
+    private String mail;
+}
--- src/main/java/com/sparta/wuzuzu/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/wuzuzu/global/security/JwtAuthenticationFilter.java
@@ -5,19 +5,24 @@ import com.sparta.wuzuzu.domain.user.dto.LoginRequest;
 import com.sparta.wuzuzu.domain.user.entity.UserRole;
 import com.sparta.wuzuzu.global.jwt.JwtUtil;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j(topic = "로그인 및 JWT 생성")
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final JwtUtil jwtUtil;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public JwtAuthenticationFilter(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
@@ -27,14 +32,14 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         try {
-            LoginRequest requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
+            LoginRequest requestDto = objectMapper.readValue(request.getInputStream(), LoginRequest.class);
 
             return getAuthenticationManager().authenticate(
-                new UsernamePasswordAuthenticationToken(
-                    requestDto.getEmail(),
-                    requestDto.getPassword(),
-                    null
-                )
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getEmail(),
+                            requestDto.getPassword(),
+                            null
+                    )
             );
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -43,12 +48,31 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         String email = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
         UserRole role = ((UserDetailsImpl) authResult.getPrincipal()).getUser().getRole();
 
-        String token = jwtUtil.createToken(email, role);
-        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+        // AccessToken 생성
+        String accessToken = jwtUtil.createAccessToken(email, role);
+
+        // RefreshToken 생성
+        String refreshToken = jwtUtil.createRefreshToken(email, role);
+
+        // 응답에 map형식으로 AccessToken, RefreshToken 추가
+        setTokenResponse(request, response, accessToken, refreshToken, email);
+    }
+
+    private void setTokenResponse(HttpServletRequest request, HttpServletResponse response, String accessToken, String refreshToken, String email) throws IOException {
+        request.setAttribute("email", email);
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("accessToken", accessToken);
+        result.put("refreshToken", refreshToken);
+
+        ResponseEntity<Map<String, Object>> responseEntity = ResponseEntity.ok(result);
+        response.getWriter().println(objectMapper.writeValueAsString(responseEntity.getBody()));
     }
 
     @Override
