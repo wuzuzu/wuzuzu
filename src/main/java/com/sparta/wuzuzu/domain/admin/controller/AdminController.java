ee42f2936b0f980e70f2f9ece5bf9a2131ce5671
9noeyni9 <koo949494@naver.com>
2024-04-04 08:27:06 +0900

    :sparkles: feat: admin 로그아웃
    - 유저 로그아웃과 기능 일치 -> 회의 후 공통화 여부 결정

--- src/main/java/com/sparta/wuzuzu/domain/admin/controller/AdminController.java
+++ src/main/java/com/sparta/wuzuzu/domain/admin/controller/AdminController.java
@@ -1,6 +1,14 @@
 package com.sparta.wuzuzu.domain.admin.controller;
 
+import com.sparta.wuzuzu.domain.admin.service.AdminService;
+import com.sparta.wuzuzu.domain.common.dto.CommonResponse;
+import com.sparta.wuzuzu.global.jwt.JwtTokenBlacklist;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/admins")
 public class AdminController {
 
-//    private final AdminService adminService;
+    private final JwtTokenBlacklist jwtTokenBlacklist;
 
+    @PostMapping("/logout")
+    public ResponseEntity<CommonResponse<String>> logout(HttpServletRequest request) {
+        String token = extractTokenFromHeader(request);
+        if (token != null) {
+            jwtTokenBlacklist.addToBlacklist(token);
+            return CommonResponse.ofDataWithHttpStatus("로그아웃 되었습니다.", HttpStatus.NO_CONTENT);
+        } else {
+            return CommonResponse.ofDataWithHttpStatus("토큰이 유효하지 않습니다.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private String extractTokenFromHeader(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+        return null;
+    }
 }
