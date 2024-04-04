5ee18b836462abb5819286a0b59736a43a9c77f6
ejchae <ejchae@kissco.co.jp>
2024-04-04 16:23:15 +0900

    ✨ 카테고리별 전체 조회기능 추가 ♻️ dto이름 변경 / 코드최적화(api호출 공통화 처리)

---
+++ src/main/java/com/sparta/wuzuzu/domain/spot/dto/response/SpotDetailResponse.java
@@ -0,0 +1,22 @@
+package com.sparta.wuzuzu.domain.spot.dto.response;
+
+import lombok.Getter;
+import org.json.JSONObject;
+
+@Getter
+public class SpotDetailResponse {
+
+    private String spotName;
+    private String address;
+    private String category;
+    private String telephone;
+    private String link;
+
+    public SpotDetailResponse(JSONObject item) {
+        this.spotName = item.getString("place_name");
+        this.address = item.getString("address_name");
+        this.category = item.getString("category_name");
+        this.telephone = item.getString("phone");
+        this.link = item.getString("place_url");
+    }
+}
