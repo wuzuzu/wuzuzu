6b5cf3518dace8a4be6b07c8bd216495a794273c
ejchae <ejchae@kissco.co.jp>
2024-04-11 03:52:45 +0900

    ✨ 로그인 / 회원가입 화면 추가

--- src/main/wuzuzu_fe/src/App.js
+++ src/main/wuzuzu_fe/src/App.js
@@ -1,45 +1,23 @@
-import logo from './logo.svg';
 import React from 'react';
-import SignUp from './components/SignUp';
-import Login from "./components/Longin";
-import {
-  BrowserRouter,
-  BrowserRouter as Router,
-  Route, Routes,
-  Switch
-} from 'react-router-dom';
-import Main from "./components/Main";
+import SignUp from './views/SignUp';
+import Login from "./views/Longin";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Main from "./views/Main";
+import Mypage from "./views/Mypage";
+import Spot from "./views/Spot";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Login/>} />
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/Main" element={<Main/>} />
+          <Route exact path="/" element={<Login/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/Main" element={<Main/>}/>
+          <Route path="/Mypage" element={<Mypage/>}/>
+          <Route path="/Spot" element={<Spot/>}/>
         </Routes>
       </BrowserRouter>
   );
 }
-
-// import './App.css';
-// import React, {useState, useEffect} from 'react';
-// function App() {
-//   const [message, setMessage]=useState([]);
-//   useEffect(()=>{
-//     fetch("/api/v1/favorites/demo-web")
-//     .then((response)=>{
-//       return response.json();
-//     })
-//     .then((data)=>{
-//       setMessage(data);
-//     });
-//   },[]);
-//   return (
-//       <div>
-//         {message}
-//       </div>
-//   );
-// }
 
 export default App;
