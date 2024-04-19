2c8afc52b712a71b7621b2bb68551cf6f6c9761a
SeungJun-Kwon <80217301+SeungJun-Kwon@users.noreply.github.com>
2024-04-19 19:11:29 +0900

    SSE 구현 (#33)
    * :sparkles: SSE Emitter 관리 Service 구현
    
    * :sparkles: 주문 시 결제 성공, 실패에 따른 SSE 알림 기능 구현

--- src/main/java/com/sparta/wuzuzu/domain/order/dto/CreateOrderMessage.java
+++ src/main/java/com/sparta/wuzuzu/domain/order/dto/CreateOrderMessage.java
@@ -14,9 +14,12 @@ public class CreateOrderMessage {
 
     private String imp_uid;
 
-    public CreateOrderMessage(User user, OrderRequest orderRequest, String imp_uid) {
+    private String merchant_uid;
+
+    public CreateOrderMessage(User user, OrderRequest orderRequest, String imp_uid, String merchant_uid) {
         this.user = user;
         this.request = orderRequest;
         this.imp_uid = imp_uid;
+        this.merchant_uid = merchant_uid;
     }
 }
--- src/main/wuzuzu_fe/src/Payment/Payment.js
+++ src/main/wuzuzu_fe/src/Payment/Payment.js
@@ -8,7 +8,7 @@ const pay_method = {
     card: 'card'
 }
 
-export const requestPay = async ({user, salePost, count}) => {
+export const requestPay = async ({user, salePost, count, handleBackClick}) => {
     const {IMP} = window;
     IMP.init(`${process.env.REACT_APP_IMP}`);
 
@@ -21,18 +21,38 @@ export const requestPay = async ({user, salePost, count}) => {
         buyer_email: user.email,
         buyer_name: user.userName,
     }, async (rsp) => {
-        try {
-            // const {data} = await axios.post(
-            //     'http://localhost:8080/verify-iamport/' + rsp.imp_uid);
-            const {data} = await createOrder({
-                salePostId: salePost.salePostId,
-                count: count,
-                amount: salePost.price * count
-            }, rsp.imp_uid);
-            console.log(data);
-        } catch (error) {
-            console.error('Error while verifying payment:', error);
-            alert('결제 실패');
+        if(rsp.success) {
+            try {
+                // const {data} = await axios.post(
+                //     'http://localhost:8080/verify-iamport/' + rsp.imp_uid);
+                const {data} = await createOrder({
+                    salePostId: salePost.salePostId,
+                    count: count,
+                    amount: salePost.price * count
+                }, rsp.imp_uid);
+
+                const eventSource = await new EventSource(
+                    `http://${window.location.hostname}:8080/api/v1/orders/${rsp.imp_uid}/sse`);
+
+                eventSource.addEventListener("success", event => {
+                    console.log(event);
+                    alert(event.data);
+                    eventSource.close();
+                    handleBackClick();
+                })
+
+                eventSource.addEventListener("failure", event => {
+                    console.log(event);
+                    alert(event.data);
+                    eventSource.close();
+                })
+            } catch (error) {
+                console.error('Error while verifying payment:', error);
+                alert('결제 실패');
+            }
+        }
+        else {
+            alert('결제 취소');
         }
     });
 };
