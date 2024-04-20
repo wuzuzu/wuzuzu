3a16ad83345597cc9f1f47e5bb172440fa813d37
ejchae <ejchae@kissco.co.jp>
2024-04-20 16:10:35 +0900

    ♻️ Main css수정

--- src/main/wuzuzu_fe/src/views/Main.js
+++ src/main/wuzuzu_fe/src/views/Main.js
@@ -2,10 +2,9 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import Header from './Header';
 import MainFeaturedPost from './MainFeaturedPost';
-import FeaturedPost from './FeaturedPost';
 import Footer from './Footer';
 import MypageMap from "../views/MypageMap";
 
@@ -15,67 +14,54 @@ const sections = [
   {title: '🐱 커뮤니티', url: '/community'}
 ];
 
-const mainFeaturedPost = {
-  title: '쭈니',
-  description:
-      "",
-  image: 'https://teamsparta.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F83c75a39-3aba-4ba4-a792-7aefe4b07895%2F7da15d97-3fa8-4417-be84-ba19d03f3cd8%2FUntitled.png?table=block&id=1fa076c8-06a3-408c-8d23-a524b00022b4&spaceId=83c75a39-3aba-4ba4-a792-7aefe4b07895&width=1150&userId=&cache=v2',
-  // imageText: 'main image description',
-  // linkText: 'Continue reading…',
-};
-
-const featuredPosts = [
+const mainFeaturedPosts = [
   {
-    title: '재롱이',
-    // date: 'Nov 12',
-    description:
-        '넘 어려워요~~ 모르겠어요~~ ',
+    image: 'https://teamsparta.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F83c75a39-3aba-4ba4-a792-7aefe4b07895%2F7da15d97-3fa8-4417-be84-ba19d03f3cd8%2FUntitled.png?table=block&id=1fa076c8-06a3-408c-8d23-a524b00022b4&spaceId=83c75a39-3aba-4ba4-a792-7aefe4b07895&width=1150&userId=&cache=v2',
+  },
+  {
     image: 'https://teamsparta.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F83c75a39-3aba-4ba4-a792-7aefe4b07895%2Ff1d77172-193a-4235-ac52-395096e19920%2FKakaoTalk_20240326_203459622.jpg?table=block&id=0224d8bd-233c-4a40-9257-48d751890520&spaceId=83c75a39-3aba-4ba4-a792-7aefe4b07895&width=2000&userId=&cache=v2',
-    // imageLabel: 'Image Text',
   },
   {
-    title: '부송',
-    // date: 'Nov 11',
-    description:
-        '흑흑 ㅜㅜ ',
     image: 'https://teamsparta.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F83c75a39-3aba-4ba4-a792-7aefe4b07895%2Fa4be6608-96ba-4621-a522-d0ffc1f60def%2FUntitled.jpeg?table=block&id=3b370cbc-e1a9-4dfd-b187-c7f79095e67c&spaceId=83c75a39-3aba-4ba4-a792-7aefe4b07895&width=2000&userId=&cache=v2',
-    // imageLabel: 'Image Text',
-  },
+  }
 ];
 
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
-export default function Main() {
+const StyledGrid = styled(Grid)({
+  '&.MuiGrid-root': {
+    border: 'none', // 그리드의 외곽선을 제거합니다.
+  },
+  '& .MuiGrid-item': {
+    borderBottom: 'none', // 그리드 아이템의 하단 선을 제거합니다.
+  },
+});
 
+export default function Main() {
   return (
       <ThemeProvider theme={defaultTheme}>
-        <CssBaseline/>
-        <Container maxWidth="lg" sx={{
-          borderBottom: 1,
-          borderColor: 'divider',
-          overflowX: 'auto'
-        }}>
-          <Header title="Wuzuzu" sections={sections}/>
-          <main>
-            <br/>
-            <MypageMap/>
-            <br/>
-            <MainFeaturedPost post={mainFeaturedPost}/>
-            <Grid container spacing={4}>
-              {featuredPosts.map((post) => (
-                  <FeaturedPost key={post.title} post={post}/>
-              ))}
-            </Grid>
-            <Grid container spacing={5} sx={{mt: 3}}>
-
-            </Grid>
-          </main>
-        </Container>
-        <Footer
-            title="Footer"
-            description="Something here to give the footer a purpose!"
-        />
+        <CssBaseline />
+        <div style={{ backgroundColor: '#FBEFEF' }}>
+          <Container maxWidth="lg" style={{ backgroundColor: '#FFFFFF' }} sx={{ borderBottom: 1, borderColor: 'divider', overflowX: 'auto' }}>
+            <Header title="Wuzuzu" sections={sections} />
+            <main>
+              <br />
+              <MypageMap />
+              <br />
+              <StyledGrid container spacing={3}>
+                {mainFeaturedPosts.map((post, index) => (
+                    <Grid item key={index} xs={12} sm={6} md={4} style={{ height: '500px' }}>
+                      <MainFeaturedPost post={post} />
+                    </Grid>
+                ))}
+              </StyledGrid>
+            </main>
+          </Container>
+          <Footer
+              title="I끼리 힘내보조"
+              description="쭈니 재롱 부송 쩹이"
+          />
+        </div>
       </ThemeProvider>
   );
 }
