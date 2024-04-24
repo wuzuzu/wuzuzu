f054547c553ba321a5714c22d796a98de3ad16bd
gorockgorock <154955856+gorockgorock@users.noreply.github.com>
2024-04-24 11:55:11 +0900

    ElasticSearch Order, Salepost 적용
    * :art: 이그노어 변경
    
    * :sparkles: 거래 게시글에 elasticsearch 적용, 제목과 상품에 특정 키워드 포함시 조회되도록, 더미데이터 삽입하는 매소드 추가 및 수정
    
    * :sparkles: 거래글의 카테고리도 키워드로 조회 가능 추가
    
    * :sparkles: 오타처리 기능 추가

---
+++ src/main/java/com/sparta/wuzuzu/domain/sale_post/dto/SalePostElasticListResponse.java
@@ -0,0 +1,20 @@
+package com.sparta.wuzuzu.domain.sale_post.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sparta.wuzuzu.global.dto.response.ListResponse;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SalePostElasticListResponse extends ListResponse {
+
+   private List<SalePostElasticResponse> responseList;
+
+}
--- src/main/java/com/sparta/wuzuzu/domain/sale_post/dto/SalePostElasticResponse.java
+++ src/main/java/com/sparta/wuzuzu/domain/sale_post/dto/SalePostElasticResponse.java
@@ -1,28 +1,32 @@
-package com.sparta.wuzuzu.domain.community_posts.dto;
+package com.sparta.wuzuzu.domain.sale_post.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.sparta.wuzuzu.domain.community_posts.entity.CommunityCategory;
+import com.sparta.wuzuzu.domain.category.entity.Category;
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.junit.experimental.categories.Categories;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true) // 알려지지 않은 필드 무시
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class CommunityElasticResponse {
-    private String title;
-    private String content;
-    private Long views;
-    private Long likeCount;
-    private Integer comments;
-    private Date timestamp;  // @timestamp 필드 추가
+public class SalePostElasticResponse {
 
+    private Long salePostId;
+    private String title;
+    private String description;
+    private Long views = 0L;
+    private Boolean status = true;
+    private String goods;
+    private Long price;
+    private Long stock;
+    private Category category;
     private User user;
-    private CommunityCategory category;
+    private Date timestamp;
 
     @Data
     public static class User {
@@ -30,7 +34,7 @@ public class CommunityElasticResponse {
     }
 
     @Data
-    public static class CommunityCategory {
+    public static class Category {
         private long categoryId;
         private String name;
     }
