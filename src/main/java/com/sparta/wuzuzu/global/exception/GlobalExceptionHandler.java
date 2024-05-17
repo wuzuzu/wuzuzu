2df38fda910b9402463ba7be71345888b3166516
DAchoMI <dachomi97@gmail.com>
2024-05-17 13:40:49 +0900

    :recycle: 전역 예외 처리에 RuntimeException, IOException 추가

--- src/main/java/com/sparta/wuzuzu/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/wuzuzu/global/exception/GlobalExceptionHandler.java
@@ -1,27 +1,21 @@
 package com.sparta.wuzuzu.global.exception;
 
 import com.sparta.wuzuzu.domain.common.dto.ExceptionResponse;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
-
-import jakarta.persistence.EntityExistsException;
-import jakarta.persistence.EntityNotFoundException;
+import java.io.IOException;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({IllegalArgumentException.class})
+
+    @ExceptionHandler({IllegalArgumentException.class, RuntimeException.class, IOException.class})
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(Exception ex) {
         ExceptionResponse response = ExceptionResponse.builder()
             .msg(ex.getMessage())
@@ -40,7 +34,8 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionResponse> handleValidationException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ExceptionResponse> handleValidationException(
+        MethodArgumentNotValidException ex) {
         ExceptionResponse response = ExceptionResponse.builder()
             .msg(Objects.requireNonNull(ex.getFieldError()).getDefaultMessage())
             .httpCode(HttpStatus.BAD_REQUEST.value())
@@ -56,14 +51,14 @@ public class GlobalExceptionHandler {
             .build();
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
+
     @ExceptionHandler(ValidateAdminException.class)
-    public ResponseEntity<ExceptionResponse> ValidationAdminException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ExceptionResponse> ValidationAdminException(
+        MethodArgumentNotValidException ex) {
         ExceptionResponse response = ExceptionResponse.builder()
             .msg(Objects.requireNonNull(ex.getFieldError()).getDefaultMessage())
             .httpCode(HttpStatus.FORBIDDEN.value())
             .build();
         return ResponseEntity.badRequest().body(response);
     }
-
-
 }
